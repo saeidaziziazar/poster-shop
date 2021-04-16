7bf3250f766ae5ae470a1c3b41fe3e14ed5e1eec
saeidaziziazar <saeid.azizi.azar@gmail.com>
2021-04-17 00:25:53 +0430

    14000127

--- public/script.js
+++ public/script.js
@@ -1,11 +1,19 @@
 new Vue({
     el: "#app",
     data : {
-        total: 0
+        total: 0,
+        products: [
+            {title: "Product One", id: 1, price: 9.99},
+            {title: "Product Two", id: 2, price: 13.99},
+            {title: "Product Three", id: 3, price: 5.99},
+            {title: "Product Four", id: 4, price: 23.99}
+        ],
+        cart: [],
     },
     methods: {
-        AddToCart: function() {
-            this.total += 9.99
+        AddToCart: function(product) {
+            this.cart.push(product);
+            this.total += product.price;
         }
     },
 })
