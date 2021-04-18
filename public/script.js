fc8e550609d9d15fff3a89a2bb8d29e011302021
saeidaziziazar <saeid.azizi.azar@gmail.com>
2021-04-19 00:02:03 +0430

    14000127

--- public/script.js
+++ public/script.js
@@ -2,18 +2,58 @@ new Vue({
     el: "#app",
     data : {
         total: 0,
-        products: [
-            {title: "Product One", id: 1, price: 9.99},
-            {title: "Product Two", id: 2, price: 13.99},
-            {title: "Product Three", id: 3, price: 5.99},
-            {title: "Product Four", id: 4, price: 23.99}
-        ],
+        products: [],
         cart: [],
+        search: "",
     },
     methods: {
         AddToCart: function(product) {
-            this.cart.push(product);
             this.total += product.price;
+            var found = false;
+            for (var i = 0; i < this.cart.length; i++) {
+                if (this.cart[i].id === product.id) {
+                    this.cart[i].qty++;
+                    found = true
+                }
+            }
+
+            if (!found){
+                this.cart.push({
+                    id: product.id,
+                    title: product.title,
+                    price: product.price,
+                    qty: 1,
+                });
+            }
+        },
+
+        inc: function(item) {
+            item.qty++;
+            this.total += item.price;
+        },
+
+        dec: function(item) {
+            item.qty--;
+            this.total -= item.price;
+
+            if (item.qty <= 0) {
+                var i = this.cart.indexOf(item);
+                this.cart.splice(i, 1);
+            }
+        },
+
+        onSubmit: function() {
+            var path = "/search?q=".concat(this.search);
+            this.$http.get(path)
+                .then(function(response) {
+                    this.products = response.body;
+                })
         }
     },
+
+    filters: {
+        currency: function(price) {
+            return "$".concat(price.toFixed(2));
+        }
+    }
 })
